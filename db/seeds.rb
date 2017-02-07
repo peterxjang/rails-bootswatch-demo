5e4f974d54bc756bb528402437338659f610a797
Peter Jang <peter.x.jang@gmail.com>
2017-02-07 13:22:47 -0600

    Add product model with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(3),
+    price: rand(5..100),
+    image: Faker::LoremPixel.image("400x300", false, 'sports')
+  )
+end
